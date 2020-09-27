663bbe659c233aaaf19b518f1b5f4963b559df22
leslie-huang <lesliehuang@nyu.edu>
2020-09-27 08:51:21 -0400

    make unfiltered data for lm training

---
+++ create_mlm_dataset.py
@@ -0,0 +1,32 @@
+import json
+import os
+import pandas as pd
+from pandas.io.json import json_normalize
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Setup sql
+engine = create_engine("sqlite:///mlm_db.db")
+
+session = sessionmaker()
+session.configure(bind=engine)
+s = session()
+
+dirname = "../congresstweets/data"
+
+# we only need tweet text and don't need to filter users!
+
+for fname in [f for f in os.listdir(dirname) if f.endswith("json")]:
+    temp_df = (
+        pd.read_json(os.path.join(dirname, fname))
+        .dropna()
+        .replace("\n", " ", regex=True)
+    )["text"]
+
+    temp_df.to_sql(
+        "mlm_data", con=engine, index=False, if_exists="append", chunksize=1000
+    )
+
+    del temp_df
+
+s.commit()
