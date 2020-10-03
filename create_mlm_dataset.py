d926bcae609d314716944d3c4338fc50305c8c07
leslie-huang <lesliehuang@nyu.edu>
2020-10-03 18:31:12 -0400

    specify paths with argparse, use to create separate test db

--- create_mlm_dataset.py
+++ create_mlm_dataset.py
@@ -1,12 +1,26 @@
 # Create a db with just tweet text and id for language model training
 
+import argparse
 import os
 import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--db_path", type=str, default="tweets.db", help="Path to SQL database"
+)
+parser.add_argument(
+    "--data_dir",
+    type=str,
+    default="../congresstweets/data/",
+    help="JSON data path",
+)
+
+args = parser.parse_args()
+
 # Setup sql
-engine = create_engine("sqlite:///hydrated_tweets.db")
+engine = create_engine(f"sqlite:///{args.db_path}")
 
 session = sessionmaker()
 session.configure(bind=engine)
@@ -15,13 +29,12 @@ s = session()
 engine.execute(
     "CREATE TABLE IF NOT EXISTS tweets(id TEXT PRIMARY KEY, text TEXT)"
 )
-dirname = "../congresstweets/data"
 
 # we only need tweet text and don't need to filter users!
 
-for fname in [f for f in os.listdir(dirname) if f.endswith("json")]:
+for fname in [f for f in os.listdir(args.data_dir) if f.endswith("json")]:
     temp_df = (
-        pd.read_json(os.path.join(dirname, fname))
+        pd.read_json(os.path.join(args.data_dir, fname))
         .dropna()
         .replace("\n", " ", regex=True)
     )[["id", "text"]]
